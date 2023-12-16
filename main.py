d7cc2e4af24f665999da2bac4f83d1d35a741f48
slohwnix <115403305+slohwnix@users.noreply.github.com>
2023-12-16 14:14:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ try:
 except ImportError:
     from Tkinter import *  # For Python 2.x
 
-class WifiConfig:
+import subprocess
+
+class WifiConfigRPi:
     def __init__(self, master):
         self.master = master
-        self.master.title("Configuration Wi-Fi")
+        self.master.title("Configuration Wi-Fi RPi")
 
         self.label_wifi = Label(self.master, text="Wi-Fi:")
         self.label_wifi.pack()
@@ -14,34 +16,30 @@ class WifiConfig:
         self.entry_wifi = Entry(self.master)
         self.entry_wifi.pack()
 
-        self.label_language = Label(self.master, text="Language:")
-        self.label_language.pack()
+        self.label_password = Label(self.master, text="Mot de passe:")
+        self.label_password.pack()
 
-        self.entry_language = Entry(self.master)
-        self.entry_language.pack()
+        self.entry_password = Entry(self.master, show="*")
+        self.entry_password.pack()
 
-        self.button_rpi = Button(self.master, text="Configure RPi", command=self.configure_rpi)
-        self.button_rpi.pack()
+        self.button_connect = Button(self.master, text="Connecter", command=self.connect_wifi)
+        self.button_connect.pack()
 
-        self.button_esp32 = Button(self.master, text="Configure ESP32", command=self.configure_esp32)
-        self.button_esp32.pack()
+    def scan_wifi_networks(self):
+        wifi_info = subprocess.run(["sudo", "iwlist", "wlan0", "scan"], capture_output=True, text=True)
+        return wifi_info.stdout
 
-    def configure_rpi(self):
+    def connect_wifi(self):
         wifi = self.entry_wifi.get()
-        language = self.entry_language.get()
-        # Code spécifique à la configuration Raspberry Pi
-        print("Configuration Raspberry Pi - Wi-Fi:", wifi, "Language:", language)
+        password = self.entry_password.get()
+        # Code pour se connecter au réseau Wi-Fi spécifié
+        print("Connecter à", wifi, "avec le mot de passe", password)
+        # À implémenter : utilisation des données pour la connexion Wi-Fi
 
-    def configure_esp32(self):
-        wifi = self.entry_wifi.get()
-        language = self.entry_language.get()
-        # Code spécifique à la configuration ESP32
-        print("Configuration ESP32 - Wi-Fi:", wifi, "Language:", language)
-
-def run_gui():
+def run_rpi_gui():
     root = Tk()
-    app = WifiConfig(root)
+    app = WifiConfigRPi(root)
     root.mainloop()
 
 if __name__ == "__main__":
-    run_gui()
+    run_rpi_gui()
